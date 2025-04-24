264729674a366e4c3d18ed6c757d780ee51b708c
Parth <parth.grover182004@gmail.com>
2025-04-24 18:03:07 +0100

    Auto-commit

--- src/components/BootUp.jsx
+++ src/components/BootUp.jsx
@@ -32,7 +32,7 @@ const BootUp = ({ onComplete }) => {
       left: 0,
       width: '100vw',
       height: '100vh',
-      margin: ','
+      margin: 'auto auto',
       backgroundColor: '#000',
       zIndex: 9999,
       color: '#00aaff',
