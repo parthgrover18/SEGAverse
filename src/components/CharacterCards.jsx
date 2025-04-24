8053affbc05e169a2a63730f8e40710e27d7f228
Parth <parth.grover182004@gmail.com>
2025-04-24 18:35:50 +0100

    Auto-commit

--- src/components/CharacterCards.jsx
+++ src/components/CharacterCards.jsx
@@ -105,7 +105,7 @@ function CharacterCards() {
             style={{
               perspective: '1000px',
               width: '20rem',
-              height: '28rem',
+              height: 'auto',
               overflow: 'visible',
             }}>
               <div style={{
