740f2b8f96331ae035f14cdfe6d60ce2325ad82c
Parth <parth.grover182004@gmail.com>
2025-04-24 21:35:24 +0100

    Auto-commit

--- src/components/CharacterCards.jsx
+++ src/components/CharacterCards.jsx
@@ -89,17 +89,17 @@ function CharacterCards() {
       </h1>
       <div className="character-card-wrapper">
         <div style={{
-          maxWidth: '1200px',
-          margin: '0 auto',
-          display: 'grid',
-          gridTemplateColumns: 'repeat(auto-fit, minmax(280px, 1fr))',
-          columnGap: '2rem',
-          justifyItems: 'center',
-          padding: '2rem',
-          alignItems: 'start',
-          alignContent: 'start',
-          textAlign: 'center'
-        }}>
+  maxWidth: '1200px',
+  margin: '0 auto',
+  display: 'grid',
+  gridTemplateColumns: 'repeat(auto-fit, minmax(280px, 1fr))',
+  columnGap: '2rem',
+  justifyItems: 'center',
+  padding: '2rem',
+  alignItems: 'start',
+  alignContent: 'start',
+  textAlign: 'center'
+}}>
         {displayedCharacters.map((char, i) => {
           return (
             <div ref={addToCardRefs} className="character-card" key={i} 
